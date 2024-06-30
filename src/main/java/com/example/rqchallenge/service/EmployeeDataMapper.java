4b545298af563dc99a7e9440a40b0695b8a79323
SSVE15\swana <swa>
2024-06-30 14:40:55 +0530

    Add a comment

--- src/main/java/com/example/rqchallenge/service/EmployeeDataMapper.java
+++ src/main/java/com/example/rqchallenge/service/EmployeeDataMapper.java
@@ -3,6 +3,11 @@ package com.example.rqchallenge.service;
 import com.example.rqchallenge.dto.Employee;
 import com.example.rqchallenge.externalapi.data.EmployeeData;
 
+/**
+ * Mapper class to map employee object returned by external API
+ * to the employee DTO that our application returns
+ * This is to avoid coupling the two 
+ */
 public class EmployeeDataMapper {
 	public static Employee mapToEmployeeDto(EmployeeData emplData) {
 		return new Employee(
